995264979c139630f2e8a2dd8783ae54e6be35d6
statuxia <me.statuxia@gmail.com>
2025-12-21 15:53:01 +0300

    (no-task): fix GameSessionIpState ACCEPTER lifetime

--- src/main/java/me/statuxia/shulkerapi/configuration/properties/AuthProperties.java
+++ src/main/java/me/statuxia/shulkerapi/configuration/properties/AuthProperties.java
@@ -7,7 +7,7 @@ public class AuthProperties {
 
     protected Long startedLifetimeSeconds = 60 * 5L; // 5 minutes
     protected Long notNotifiedLifetimeSeconds = 0L; // instant reset
-    protected Long acceptedLifetimeSeconds = 60 * 60 * 24 * 7L; // 7 days
+    protected Long acceptedLifetimeSeconds = 60 * 5L; // 5 minutes
     protected Long rejectedLifetimeSeconds = 60 * 60 * 24 * 31L; // 31 days
     protected Long outdatedLifetimeSeconds = 0L; // instant reset
 
