99b900cc91411267dd5a8bd493b33961c4d01456
feeland <gugber37@gmail.com>
2026-02-14 06:28:07 +0300

    SWA-22 require changes

--- src/main/java/me/statuxia/shulkerapi/dao/impl/AccountRoleDAO.java
+++ src/main/java/me/statuxia/shulkerapi/dao/impl/AccountRoleDAO.java
@@ -3,12 +3,17 @@ package me.statuxia.shulkerapi.dao.impl;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
+import jakarta.transaction.Transactional;
 import me.statuxia.shulkerapi.dto.search.impl.AccountRoleSearchDTO;
 import me.statuxia.shulkerapi.model.AccountRole;
+import org.joda.time.DateTime;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
+@Transactional
 public class AccountRoleDAO extends BaseDAO<AccountRole, Long, AccountRoleSearchDTO> {
     @Override
     public Predicate buildPredicate(
@@ -23,6 +28,10 @@ public class AccountRoleDAO extends BaseDAO<AccountRole, Long, AccountRoleSearch
         }
 
         predicates.add(cb.equal(root.get("account"), searchDTO.getAccount()));
+
+        if (searchDTO.isActive()) {
+            predicates.add(cb.greaterThanOrEqualTo(root.get("expireDate"), DateTime.now()));
+        }
 
         return cb.and(predicates.toArray(new Predicate[0]));
     }
